9872a4e8738f79af466019a96f0c4bab2211748f
surfmuggle <1271555+surfmuggle@users.noreply.github.com>
2026-02-17 08:28:44 +0000

    first http server

--- main.go
+++ main.go
@@ -1,11 +1,29 @@
 package main
 
-import "fmt"
+import (
+		"fmt"
+		"log"
+		"net/http"
+)
 
-func main(){
-	fmt.Println("Hallo") 
+func main() {
+		http.HandleFunc("/", helloHandler)
+		addr := ":8080"
+		fmt.Println("Starting server on", addr)
+		log.Fatal(http.ListenAndServe(addr, nil))
 }
 
-func getName(n string) string {
-	return n
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		fmt.Fprint(w, `<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Welcome</title>
+	</head>
+	<body>
+		<h2>welcome to github.dev</h2>
+		<p>hello word</p>
+	</body>
+</html>`)
 }
